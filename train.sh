f1d1260f739f63daec1f4672439c23367f22b2fe
fang <fang@mac13>
2020-07-25 22:44:33 +0800

    test

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 python train.py \
-	--stride 40 \
+	--stride 384 \
 	--model_config config/model-dev.json \
 	--data_dir data/tokenized \
 	--overwrite_output_dir \
 	--do_train \
 	--num_train_epochs 1 \
-	--save_steps 5 \
+	--save_steps 15 \
 	--output_dir data/model \
-	--save_total_limit 3 \
-	--per_device_train_batch_size 2 \
+	--save_total_limit 1 \
+	--per_device_train_batch_size 4 \
 	--vocab_file data/vocab/clue_vocab.txt
 	#--pretrained_model_path model/model_epoch_1
