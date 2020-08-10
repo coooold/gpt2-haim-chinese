954d7a1c69e7f36ef788e3712cd11af20c790ebf
fang <fang@mac>
2020-08-10 14:42:49 +0800

    simple train

--- utils/dataset.py
+++ utils/dataset.py
@@ -15,54 +15,25 @@ class GPT2Dataset(Dataset):
                  ):
         self.begin_token_id = tokenizer.convert_tokens_to_ids('<begin>')
         self.end_token_id = tokenizer.convert_tokens_to_ids('<end>')
+        self.cls_token_id = tokenizer.convert_tokens_to_ids('[CLS]')
         self.pad_token_id = 0
         self.n_ctx = n_ctx
         self.stride = stride
         self.tokenizer = tokenizer
 
-        self.train_data = []
-        self.train_data_index = []
-        self.train_data_count = 0
-
-        self.tokenizer = tokenizer
-        self.sep_token_ids = self.tokenizer.convert_tokens_to_ids(
-            self.tokenizer.tokenize('。，；？！[SEP]')
-        )
-        index = 0
+        self.data = []
         for f in self.scan_files(tokenized_file_path):
             print("load training {}".format(f))
             raw = np.fromfile(f, dtype=np.int16).tolist()
-            sample_count = (len(raw) - self.n_ctx) // self.stride
-            index += sample_count
-            self.train_data.append(raw)
-            self.train_data_index.append(index)
+            self.data += raw + [self.cls_token_id]
 
     def __len__(self):
-        return self.train_data_index[len(self.train_data_index) - 1]
+        return (len(self.data) - self.n_ctx) // self.stride
 
     def __getitem__(self, i):
-        # 找到i在哪个文件中
-        j = 0
-        for idx, num in enumerate(self.train_data_index):
-            if i < num:
-                j = idx
-                break
-        start = 0 if j == 0 else self.train_data_index[j-1]
-        raw = self.train_data[j]
-        pos = i - start
-
-        sample = raw[self.stride * pos: self.stride * pos + self.n_ctx]
+        pos = self.stride * i
+        sample = self.data[pos: pos + self.n_ctx]
         return self.process_sample(sample)
-
-    def to_sentences(self, s):
-        sentences = []
-        sentence = []
-        for i, t in enumerate(s):
-            sentence.append(t)
-            if self.sep_token_ids.__contains__(t):
-                sentences.append(sentence)
-                sentence = []
-        return sentences
 
     @staticmethod
     def scan_files(tokenized_data_path: str):
@@ -74,63 +45,14 @@ class GPT2Dataset(Dataset):
         return train_files
 
     def process_sample(self, arr):
-        # print("loading tokenized file: {}".format(tokenized_file))
+        sample_len = self.n_ctx
+        prefix_len = randint(1, sample_len * 0.3)
+        prefix = arr[0:prefix_len]
+        suffix = arr[prefix_len:sample_len]
 
-        min_len = 32
-        max_suffix_sentences = 3
-        sample_len = randint(min_len, self.n_ctx - 6)
-        sample = arr[:sample_len]
-
-        sentences = self.to_sentences(sample)
-        sentences_len = len(sentences)
-
-        if sentences_len < 3:
-            suffix_sentence_count = 0
-        else:
-            suffix_sentence_count = randint(1, min(len(sentences) - 1, max_suffix_sentences))
-        prefix_sentence_count = len(sentences) - suffix_sentence_count
-
-        prefix_sentences = sentences[:prefix_sentence_count]
-        suffix_sentences = sentences[prefix_sentence_count:]
-
-        prefix_tokens = []
-        for i in prefix_sentences:
-            prefix_tokens.extend(i)
-        suffix_tokens = []
-        for i in suffix_sentences:
-            suffix_tokens.extend(i)
-
-        # if there is a suffix
-        if len(suffix_tokens) > 0:
-            suffix_tokens.append(self.begin_token_id)
-        prefix_tokens.append(self.end_token_id)
-        suffix_len = len(suffix_tokens)
-        prefix_len = len(prefix_tokens)
-
-        """
-        # The random shift \deltaδ is introduced to soften the length constraint, 
-        effectively allowing the model some leeway at inference time. 
-        We sampled the position shift uniformly in \left[0, 0.1\times n\right][0,0.1×n].
-        """
-        delta = int((random() - 0.5) * 7)  # [-3, 3]
-        suffix_positions = [prefix_len + 1 + i + delta for i in range(suffix_len)]
-        prefix_positions = [i for i in range(len(prefix_tokens))]
-
-        # All labels set to ``-100`` are ignored (masked), the loss is only
-        #             computed for labels in ``[0, ..., config.vocab_size]``
-        # modeling_gpt2.py
-        final_sample = suffix_tokens + prefix_tokens
-        final_sample_positions = suffix_positions + prefix_positions
-        final_label = [-100] * suffix_len + prefix_tokens
-
-        # padding
-        if len(final_sample) < self.n_ctx:
-            final_sample = final_sample + [self.pad_token_id] * (self.n_ctx - len(final_sample))
-            final_sample_positions = final_sample_positions + [0] * (self.n_ctx - len(final_sample_positions))
-            final_label = final_label + [-100] * (self.n_ctx - len(final_label))
+        final_sample = suffix + [self.begin_token_id] + prefix + [self.end_token_id]
 
         return {
             'input_ids': final_sample,
-            'labels': final_label,
-            'position_ids': final_sample_positions,
+            'labels': final_sample,
         }
